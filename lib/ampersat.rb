2a49915640c24afd3b7edf845b890e188e7144f7
Gareth Rees <reesG12@cardiff.ac.uk>
2011-11-28 16:16:30 +0000

    Commit some stuff

--- lib/ampersat.rb
+++ lib/ampersat.rb
@@ -1,5 +1,23 @@
 require "ampersat/version"
+require "csv"
 
 module Ampersat
-  # Your code goes here...
+
+  def self.domains(email_list)
+    domains = Hash.new(0)
+    
+    CSV.foreach(email_list) do |row|
+      domain = Ampersat.find_domain(row.first)
+      domains[domain] = domains[domain] +1
+    end
+
+    # return domains
+    domains.sort_by {|key, value| -value}
+    
+  end
+
+  def self.find_domain(email)
+    email.split('@').last
+  end
+
 end
--- spec/ampersat_spec.rb
+++ spec/ampersat_spec.rb
@@ -7,6 +7,22 @@ require "ampersat"
 describe Ampersat do
 
   let(:email) { "test@example.com" }
-  let(:multiple_emails) { "jim@example.com, bob@example.org, james@example.net" }
+  let(:multiple_emails) { "jim@example.com, tom@example.com, bob@example.org, james@example.net" }
+
+  it "should return the sum of each email provider" do
+    expected_result = { "example.com" => 2, "example.org" => 1, "example.net" => 1 }
+    Ampersat.domains(multiple_emails).should == expected_result
+  end
+
+  it "should return the email providers with the most common first" do
+    incorrect_result = { "example.org" => 1, "example.com" => 2, "example.net" => 1 }
+    Ampersat.domains(multiple_emails).should_not == expected_result
+    expected_result = { "example.com" => 2, "example.org" => 1, "example.net" => 1 }
+    Ampersat.domains(multiple_emails).should == expected_result
+  end
+
+  it "should find the domain of an email address" do
+    Ampersat.find_domain(email).should == "example.com"
+  end
 
 end
