cf34ad845d1685978cbc8cf51ab74d6cceb428fd
Robert Coleman <github@robert.net.nz>
2014-05-30 14:26:19 +1200

    add support for looking up mx records

--- ampersat.gemspec
+++ ampersat.gemspec
@@ -6,8 +6,8 @@ require 'ampersat/version'
 Gem::Specification.new do |s|
   s.name        = 'ampersat'
   s.version     = Ampersat::VERSION
-  s.authors     = ['Gareth Rees']
-  s.email       = ['gareth.h.rees@gmail.com']
+  s.authors     = ['Gareth Rees', 'Robert Coleman']
+  s.email       = ['gareth.h.rees@gmail.com', 'github@robert.net.nz']
   s.homepage    = 'http://github.com/garethrees/ampersat'
   s.summary     = 'Calculates which email domains your subscribers use'
   s.description = 'Calculates which email domains your subscribers use'
@@ -22,5 +22,8 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rake'
   s.add_development_dependency 'rspec', '~> 2.7'
   s.add_dependency 'thor', '~> 0.19.1'
-
+  s.add_dependency 'public_suffix', '~> 1.4.4'
+  s.add_dependency 'ruby-progressbar', '~> 1.5.1'
+  s.add_dependency 'celluloid', '0.15.2'
+  s.add_dependency 'celluloid-pmap', '~> 0.1.0'
 end
--- lib/ampersat.rb
+++ lib/ampersat.rb
@@ -1,4 +1,8 @@
 require 'csv'
+require 'public_suffix'
+require 'ruby-progressbar'
+require 'resolv'
+require 'celluloid/pmap'
 
 module Ampersat
 
@@ -13,8 +17,37 @@ module Ampersat
     domains.sort_by {|key, value| - value}
   end
 
+  def self.mxs(email_list, offset=0)
+    mxs = {}
+    domains = Ampersat.domains(email_list, offset)
+    progressbar = ProgressBar.create(format: '%t: %c / %C |%B|', starting_at: 0, total: domains.length)
+    domains.pmap(100) do |domain, qty|
+      mx = Ampersat.find_mx(domain)
+      mxs[mx] ||= {}
+      mxs[mx]['domains'] ||= []
+      mxs[mx]['domains'] << domain
+      mxs[mx]['addresses'] ||= 0
+      mxs[mx]['addresses'] = mxs[mx]['addresses'] + qty
+      progressbar.increment
+    end
+
+    mxs.sort_by {|key, value| - value['addresses'] }
+  end
+
   def self.find_domain(email)
-    email.split('@').last
+    email.split('@').last.downcase
+  end
+
+  def self.find_mx(domain)
+    @mxs ||= {}
+    @mxs[domain] ||= begin
+      Resolv::DNS.open(nameserver: '8.8.8.8') do |dns|
+        next unless PublicSuffix.valid?(domain)
+        response = dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
+        next unless response.any?
+        response.collect { |r| r.exchange.to_s.downcase }.sort.first
+      end
+    end
   end
 
 end
